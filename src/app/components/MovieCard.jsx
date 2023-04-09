6afbe0f1f2c35423767daa1b8abc417c90c68f36
abinash.shasini <abinashshasini125@gmail.com>
2023-04-09 11:27:42 +0530

    Movie card css completed

--- src/app/components/MovieCard.jsx
+++ src/app/components/MovieCard.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 'use client';
-import Image from 'next/image';
 import { useEffect, useRef } from 'react';
 import { BsThreeDots } from 'react-icons/bs';
 import { IMAGE_BASE_URL, POSTER_SIZE } from '../config';
+import Image from 'next/image';
 import styles from '../styles/MovieCard.module.css';
 
 const MovieCard = ({
@@ -14,15 +14,32 @@ const MovieCard = ({
   key,
 }) => {
   // * Required states and refs * //
-  const circleAnimationRef = useRef(null);
+  const circleOneRef = useRef(null);
+  const circleTwoRef = useRef(null);
 
   // * Effect to calculate the total length of circle * //
   useEffect(() => {
-    const length = circleAnimationRef.current.getTotalLength();
+    const length = circleTwoRef.current.getTotalLength();
     const _percentage = Math.floor((Number(percentage) * 100) / 10);
-    circleAnimationRef.current.style.strokeDashoffset = 0;
-    circleAnimationRef.current.style.strokeDasharray =
-      (_percentage / 100) * 100;
+    circleTwoRef.current.style.strokeDasharray = length;
+    // circle color code
+    if (_percentage <= 20) {
+      circleOneRef.current.style.stroke = '#d40242';
+      circleTwoRef.current.style.stroke = '#d40242';
+    } else if (_percentage > 20 && _percentage <= 60) {
+      circleOneRef.current.style.stroke = '#d29001';
+      circleTwoRef.current.style.stroke = '#d29001';
+    } else {
+      circleOneRef.current.style.stroke = '#00e55b';
+      circleTwoRef.current.style.stroke = '#00e55b';
+    }
+    // circle fill percentage code
+    if (percentage !== 0) {
+      circleTwoRef.current.style.strokeDashoffset =
+        100 - (_percentage / 95) * 100;
+    } else {
+      circleTwoRef.current.style.strokeDashoffset = length;
+    }
   }, []);
 
   return (
@@ -40,19 +57,23 @@ const MovieCard = ({
       </div>
       <div className={styles.textContainer}>
         <div className={styles.percentageContainer}>
-          {percentage && (
-            <div className={styles.percentage}>
-              {Math.floor((Number(percentage) * 100) / 10)}%
-            </div>
-          )}
+          <div className={styles.percentage}>
+            {percentage ? Math.floor((Number(percentage) * 100) / 10) : 0}%
+          </div>
           <svg width="40" height="40">
-            <circle className={styles.circle1} cx="17" cy="20" r="15"></circle>
+            <circle
+              className={styles.circle1}
+              cx="17"
+              cy="20"
+              r="15"
+              ref={circleOneRef}
+            ></circle>
             <circle
               className={styles.circle2}
               cx="17"
               cy="20"
               r="15"
-              ref={circleAnimationRef}
+              ref={circleTwoRef}
             ></circle>
           </svg>
         </div>
