5d70d071c0293a54657cb922b6452c57afe2ac40
abinash.shasini <abinashshasini125@gmail.com>
2023-04-09 18:59:49 +0530

    Infinite scroll added

--- src/clientComponents/ResultsClientCmp.jsx
+++ src/clientComponents/ResultsClientCmp.jsx
@@ -1,10 +1,47 @@
 'use client';
+import { useState, useRef, useEffect } from 'react';
 import MovieCard from '@/components/MovieCard';
-import { useState } from 'react';
+import useInfiniteScroll from '@/hooks/useInfiniteScroll';
 import styles from '../styles/Results.module.css';
-
+import API from '@/api';
 const ResultsClientCmp = ({ data: dataFromServer, type }) => {
+  // * Required states and refs * //
   const [data, setData] = useState(dataFromServer);
+  const [page, setPage] = useState(2);
+  const loadMoreRef = useRef(null);
+  const { isIntersecting = false } = useInfiniteScroll(loadMoreRef, {
+    threshold: 0,
+    root: null,
+    rootMargin: '400px',
+  });
+
+  // * Function to fetch data * //
+  const handleFetchData = async () => {
+    try {
+      const response = await API.fetchWhatsPopular(type, page);
+      if (response && response.results && response.results.length > 0) {
+        setData([...data, ...response.results]);
+      }
+    } catch (error) {
+      console.error(
+        'Something occure while fetching popular movie response',
+        error
+      );
+    }
+  };
+
+  // * Effect for page increment on intersection * //
+  useEffect(() => {
+    if (isIntersecting) {
+      setPage(page + 1);
+    }
+  }, [isIntersecting]);
+
+  // * Effect to call API when page changes * //
+  useEffect(() => {
+    console.log('pages');
+    handleFetchData();
+  }, [page]);
 
   return (
     <div className={styles.container}>
@@ -22,6 +59,9 @@ const ResultsClientCmp = ({ data: dataFromServer, type }) => {
               percentage={element.vote_average}
             />
           ))}
+      </div>
+      <div data-observe={true} ref={loadMoreRef}>
+        loading
       </div>
     </div>
   );
