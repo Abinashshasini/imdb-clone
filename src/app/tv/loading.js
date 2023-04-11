d1d6be79633ce94dc721790ecfb32aa4c83e8336
abinash.shasini <abinashshasini125@gmail.com>
2023-04-11 21:38:38 +0530

    Tv page fix

--- src/app/tv/loading.js
+++ src/app/tv/loading.js
@@ -1,5 +1,27 @@
 import CardSkeleton from '../../skeleton/CardSkeleton';
+import HorizontalSkeleton from '../../skeleton/HorizontalSkeleton';
 
-export default function Loading() {
-  return <CardSkeleton type="large" />;
-}
+const Loading = () => {
+  return (
+    <div className="px-5 py-5 lg:px-48 lg:py-10 md:px-20 ">
+      <div
+        style={{
+          gridTemplateColumns: 'repeat(auto-fill, minmax(180px, 1fr))',
+          gap: '2rem',
+        }}
+        className="hidden md:grid mt-10 pb-20"
+      >
+        {new Array(16).fill(1).map((_) => (
+          <CardSkeleton type="large" />
+        ))}
+      </div>
+      <div className="flex mt-10 pb-20 lg:hidden  flex-wrap">
+        {new Array(16).fill(1).map((_) => (
+          <HorizontalSkeleton />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
