76d19f6968dc33d5daddaaf7d3afd5fb7fc6c29a
abinash.shasini <abinashshasini125@gmail.com>
2023-04-12 10:30:48 +0530

    Tap heilight removed

--- src/api.js
+++ src/api.js
@@ -56,8 +56,13 @@ const apiSettings = {
     return await (await fetch(endpoint)).json();
   },
 
+  fetchMovieVideo: async (movieId) => {
+    const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+    return await (await fetch(endpoint)).json();
+  },
+
   fetchCredits: async (movieId) => {
-    const creditsEndpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+    const creditsEndpoint = `${API_URL}movie/${movieId}/videos?api_key=${API_KEY}`;
     return await (await fetch(creditsEndpoint)).json();
   },
 
