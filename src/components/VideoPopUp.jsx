5191bfd62baa1b6e5eeb0de30e801438d9135921
abinash.shasini <abinashshasini125@gmail.com>
2023-04-25 15:36:11 +0530

    Movie and tv page complted TG-I11008-P60

--- src/components/VideoPopUp.jsx
+++ src/components/VideoPopUp.jsx
@@ -1,9 +1,14 @@
 import ReactPlayer from 'react-player';
+import { useEffect } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import styles from '../styles/VideoPopUp.module.css';
-import { useEffect } from 'react';
 
-const VideoPopup = ({ setShowTrailer, trailerKey, setTrailerKey }) => {
+const VideoPopup = ({
+  setShowTrailer,
+  trailerKey,
+  setTrailerKey,
+  trailerName,
+}) => {
   // * Function to hide pop video pop up * //
   const handleHidePopup = (e) => {
     e.stopPropagation();
@@ -22,15 +27,23 @@ const VideoPopup = ({ setShowTrailer, trailerKey, setTrailerKey }) => {
   return (
     <div className={styles.container} onClick={(e) => handleHidePopup(e)}>
       <div className={styles.videoCnt}>
-        <span className={styles.closeIcon} onClick={(e) => handleHidePopup(e)}>
-          <AiOutlineClose />
-        </span>
-        <ReactPlayer
-          url={`https://www.youtube.com/watch?v=${trailerKey}`}
-          controls
-          width="100%"
-          height="100%"
-        />
+        <div className={styles.videoNameCnt}>
+          <h1>{trailerName}</h1>
+          <span
+            className={styles.closeIcon}
+            onClick={(e) => handleHidePopup(e)}
+          >
+            <AiOutlineClose />
+          </span>
+        </div>
+        <div className={styles.playerCnt}>
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${trailerKey}`}
+            controls
+            width="100%"
+            height="100%"
+          />
+        </div>
       </div>
     </div>
   );
